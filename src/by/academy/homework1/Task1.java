23b25fc5fe2040921cfbc75a7e2800db1968f645
a-viator <evgeniy.palaus@gmail.com>
2022-11-25 22:30:25 +0300

    tasks 1 and 2 from homework complete

---
+++ src/by/academy/homework1/Task1.java
@@ -0,0 +1,56 @@
+/*
+ * Задание 1.
+ *
+ * Ввести с консоли сумму покупки в магазине и возраст покупателя,
+ * в зависимости от суммы вывести на экран ФИНАЛЬНУЮ ЦЕНУ с учетом скидки.
+ * Скидки:
+ *	1) Сумма до 100 рублей -> 5%
+ *	2) Сумма от 100 рублей включая до 200 рублей не включая -> 7%
+ *	3) Сумма от 200 рублей включая до 300 рублей не включая -> 12%
+ *	4) Сумма от 300 рублей включая до 400 рублей не включая -> 15%
+ *	5) Сумма покупки больше 400 рублей включая -> 20%
+ * В случае, если сумма покупки находится в 3 интервале (сумма от 200 рублей включая до 300 рублей не включая)
+ * и возраст покупателя больше, чем 18 лет -> добавить 4% к скидке (12 + 4), иначе -> отнять 3%.
+ */
+
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+
+		int age;                                            //age of the customer
+		double price;                                       //purchase price without discount
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Введите свой возраст: ");
+		age = scan.nextInt();
+		System.out.print("Введите стоимость покупок: ");
+		price = scan.nextDouble();
+		scan.close();
+
+		if (price < 100) {
+			price = price - price * 0.05;
+			System.out.println("С Вас " + price + " рублей");
+		} else if (price >= 100 && price < 200) {
+			price = price - price * 0.07;
+			System.out.println("С Вас " + price + " рублей");
+		} else if (price >= 200 && price < 300) {
+			if (age > 18) {
+				price = price - price * 0.16;
+				System.out.println("С Вас " + price + " рублей");
+			} else {
+				price = price - price * 0.09;
+				System.out.println("С Вас " + price + " рублей");
+			}
+		} else if (price >= 300 && price < 400) {
+			price = price - price * 0.15;
+			System.out.println("С Вас " + price + " рублей");
+		} else if (price >= 400) {
+			price = price - price * 0.2;
+			System.out.println("С Вас " + price + " рублей");
+		}
+	}
+}
---
+++ src/by/academy/homework1/Task2.java
@@ -0,0 +1,78 @@
+/*
+ * Задание 2.
+ *
+ * Используем switch/case. Ввести с консоли тип данных(созраняем в String) и переменную, вывести на экран:
+ *	a) если тип данных int, вывести остаток от деления на 2
+ *	b) если double, вывести 70% от числа
+ *	c) если float, возвести в квадрат
+ *	d) если char, вывести код символа (charAt(0))
+ *	e) String, вывести на экран строку в виде ("Hello " + переменная)
+ *	f) во всех остальных случаях вывести на экран ("Unsupported type");
+ */
+
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		int input1;
+		double input2;
+		float input3;
+		String input4;
+		String input5;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Введите тип данных: ");
+		scan = new Scanner(System.in);
+		String type = scan.nextLine();
+
+		switch (type) {
+			case "int":
+				System.out.print("Введите значение переменной: ");
+				if (scan.hasNextInt()){
+					input1 = scan.nextInt();
+					input1 %= 2;
+					System.out.println("Остаток от деления на 2 равен " + input1);
+				} else {
+					System.out.println("Введите int переменную!");
+				}
+				break;
+			case "double":
+				System.out.print("Введите значение переменной: ");
+				if (scan.hasNextDouble()) {
+					input2 = scan.nextDouble();
+					input2 = input2 * 0.7;
+					System.out.println("70% от числа равны: " + input2);
+				} else {
+					System.out.println("Введите double переменную!");
+				}
+				break;
+			case "float":
+				System.out.print("Введите значение переменной: ");
+				if (scan.hasNextFloat()) {
+					input3 = scan.nextFloat();
+					input3 *= input3;
+					System.out.println("Квадрат числа равен: " + input3);
+				} else {
+					System.out.println("Введите float переменную!");
+				}
+				break;
+			case "char":
+				System.out.print("Введите значение переменной: ");
+				input4 = scan.nextLine();
+				System.out.print("Первым символом был " + input4.charAt(0));
+				break;
+			case "String":
+				System.out.print("Введите значение переменной: ");
+				input5 = scan.nextLine();
+				System.out.print("Hello " + input5);
+				break;
+			default:
+				System.out.println("Unsupported type");
+		}
+		scan.close();
+	}
+}
