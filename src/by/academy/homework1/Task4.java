86fac27107c46d6f41350f6c734c6b8335a14af2
a-viator <evgeniy.palaus@gmail.com>
2022-11-28 13:36:24 +0300

    Task 4 is done

---
+++ src/by/academy/homework1/Task4.java
@@ -0,0 +1,35 @@
+/*
+ * Задание 4.
+ *
+ * Выводить на консоль степень двойки, пока результат не будет больше, чем 1_000_000.
+ */
+
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+
+		int a;
+		int b;
+		int power;
+
+		System.out.println("Введите число: ");
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextInt();
+		scan.close();
+
+		b = a;
+
+		for (power = 1; b <= 1_000_000; power++) {
+			if (power == 1) {
+				System.out.println(a + " в степени " + power + " равен " + b);
+			} else {
+				b = b * a;
+				System.out.println(a + " в степени " + power + " равен " + b);
+			}
+		}
+	}
+}
