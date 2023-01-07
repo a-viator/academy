4e26d0bcea911a7cacb32bc4c7c50ba84b4d0e88
a-viator <evgeniy.palaus@gmail.com>
2023-01-07 18:23:17 +0300

    Task 2 probably done.

---
+++ src/by/academy/homework4/task2/Body.java
@@ -0,0 +1,46 @@
+package by.academy.homework4.task2;
+
+public class Body {
+
+	private Heart heart;
+	private Lungs lungs;
+
+
+	public Body() {
+		super();
+		this.heart = new Heart();
+		this.lungs = new Lungs();
+	}
+
+	public class Heart {
+		public void live() {
+			for (int i = 0; i < Integer.MAX_VALUE; i++) {
+				if (i != 0 && i % 100 == 0) {
+					System.out.println("Knock-knock-knockin' on heaven's door " + i);
+				}
+			}
+		}
+	}
+
+	class Lungs {
+		public void live() {
+			for (int i = 0; i < Integer.MAX_VALUE; i++) {
+				if (i != 0 && i % 50 == 0) {
+					System.out.println("breathe in " + i);
+				}
+				if (i != 0 && i % 100 == 0) {
+					System.out.println("breathe out " + i);
+				}
+			}
+		}
+	}
+
+	public void live() {
+		lungs.live();
+		heart.live();
+		System.out.println("""
+				You stand on the shore and feel the salty smell of the wind that blows from the sea.
+				And I believe that you are free, and life has only begun.
+				""");
+	}
+}
---
+++ src/by/academy/homework4/task2/BodyApp.java
@@ -0,0 +1,10 @@
+package by.academy.homework4.task2;
+
+public class BodyApp {
+
+	public static void main(String[] args) {
+
+		Body body = new Body();
+		body.live();
+	}
+}
