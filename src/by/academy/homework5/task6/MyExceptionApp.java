dd37a7b213bbc619978e4afd67aed727128b7e33
a-viator <evgeniy.palaus@gmail.com>
2023-01-16 11:09:12 +0300

    Task6 completed

---
+++ src/by/academy/homework5/task6/MyExceptionApp.java
@@ -0,0 +1,31 @@
+/*
+ * Задание 6.
+ *
+ * Задача 6.
+ * Попробовать добавить в массив int на 8 элементов 10 чисел.
+ * Обернуть в try/catch, словить ArrayIndexOutOfBoundsException и вывести на экран сообщение:
+ * "Array is to small, expand the array".
+ */
+
+package by.academy.homework5.task6;
+
+import java.util.Arrays;
+
+public class MyExceptionApp {
+
+	public static void main(String[] args) {
+
+		int[] array = new int[8];
+
+		try {
+			array[1] = 1;
+			for (int i = 0; i < 10; i++) {
+				array[i] = i + 1;
+			}
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println("Array is to small, expand the array");
+		} finally {
+			System.out.println(Arrays.toString(array));
+		}
+	}
+}
